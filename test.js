7465db1838823b9fc7fdf530e05678bd58086f79
ariefardi <forzaariefpyon@gmail.com>
2018-07-23 14:08:11 +0700

    dlete test

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const expect = chai.expect
 const chaiHttp = require('chai-http')
 chai.use(chaiHttp)
 let url = 'http://localhost:3000'
+let id = ''
 
 describe('get article', function(){
     it('expect to have status 200', function(done){
@@ -105,3 +106,77 @@ describe('get article', function(){
         })
     })
 })
+
+describe('post article',function(){
+    before(function(done){
+        chai.request(url)
+        .post('/users/login')
+        .send({
+            '_method' : 'post',
+            'username' : 'loki',
+            'password' : '08november'
+        })
+        .end(function(err,res){
+            expect(res).to.have.status(200)
+            done()
+        })
+    })
+    it('post article succes', function(done){
+        chai.request(url)
+        .post('/articles')
+        .send({
+            '_method' : 'post',
+            'title' : 'ini title',
+            'content' : 'ini content',
+            'category' : 'ini category',
+            'author' : 'loki',
+            'imgSrc' : 'img src',
+            'date' : 'July 9th 2018',
+            'realContent' : 'real content here'
+        })
+        .end(function(err,res){
+            let article = res.body
+            expect(res).to.have.status(200)
+            expect(article).to.have.property('title')
+            expect(article).to.have.property('content')
+            expect(article).to.have.property('category')
+            expect(article).to.have.property('author')
+            expect(article).to.have.property('imgSrc')
+            expect(article).to.have.property('date')
+            expect(article).to.have.property('realContent')
+            console.log(res.body, ' ini res body')
+            done()
+        })
+    })
+})
+
+// describe('delete Article',function(){
+//     console.log('delete')
+//     this.timeout(7000)
+//     before(function(done){
+//         console.log('before delete')
+//         chai.request(url)
+//         .post('/users/login')
+//         .type('form')
+//         .send({
+//             '_method' : 'post',
+//             'username' : 'loki',
+//             'password' : '08november'
+//         })
+//         .end(function(err,res){
+//             expect(res).to.have.status(200)
+//             done()
+//         })
+//     })
+
+//     it('delete article',function(done){
+//         console.log('masuk delete')
+//         console.log(id,'ini id')
+//         chai.request(url)
+//         .delete('/articles/delete/:id')
+//         .end(function(err,res){
+//             console.log(id)
+//             done()
+//         })
+//     })
+// })
